0000d80002696839ebeed2e545c846593b7d0a17
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-27 11:44:38 +0530

    adding API Gateway

--- app/hello-world/app.js
+++ app/hello-world/app.js
@@ -1,33 +1,38 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
-let response;
+const htmlResponse = require('./html-response');
+const formHtml = `
+  <html>
+  <head>
+    <meta charset="utf-8"/>
+  </head>
+  <body>
+    <form method="POST">
+      Please enter your name:
+      <input type="text" name="name"/>
+      <br/>
+      <input type="submit" />
+    </form>
+  </body>
+  </html>
+`;
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
+const thanksHtml = `
+  <html>
+  <head>
+    <meta charset="utf-8"/>
+  </head>
+  <body>
+    <h1>Thanks</h1>
+    <p>We received your submission</p>
+  </body>
+  </html>
+`;
+
 exports.lambdaHandler = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  console.log(JSON.stringify(event, null, 2));
 
-    return response
+  if (event.httpMethod === 'GET') {
+    return htmlResponse(formHtml);
+  } else {
+    return htmlResponse(thanksHtml);
+  }
 };
